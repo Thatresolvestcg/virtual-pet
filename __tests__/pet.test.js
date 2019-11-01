d8d834db31e3797112ea34b0aea86552bfe09334
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-01 22:03:12 +0000

    added a test for an initial age of 0

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -8,4 +8,8 @@ describe("constructor", () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual("Fido");
     });
+    test("it has an initial age of 0", () => {
+        const pet = new Pet("Fido");
+        expect(pet.age).toEqual(0);
+    })
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,6 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
 }
 
 module.exports = Pet;
